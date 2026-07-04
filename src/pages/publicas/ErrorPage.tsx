d2eb8a2d3df8c6ecd449d95d9a30308d52e0bfc3
KealPetu <kpenafiel99@gmail.com>
2026-07-04 18:25:40 -0500

    ErrorPage created

---
+++ src/pages/publicas/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import './ErrorPage.css';
+
+export default function ErrorPage() {
+  return (
+    <div className="error-page-404">
+      <h1 className="code">404</h1>
+      <h2>PAGE NOT FOUND!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.
+      </p>
+      <a href="#" className="btn">Inicio</a>
+    </div>
+  );
+}
