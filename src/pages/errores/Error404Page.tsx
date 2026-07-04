7db714cbfaf8a4f121e0b6dc52e35c0e443c9d06
KealPetu <kpenafiel99@gmail.com>
2026-07-04 18:26:14 -0500

    Error404 page component created

---
+++ src/pages/errores/Error404Page.tsx
@@ -0,0 +1,14 @@
+export default function Error404Page() {
+  return (
+    <>
+      <nav>
+        <h1>VotoSeguro</h1>
+      </nav>
+
+      <h2>404 - Página no encontrada</h2>
+      <p>La página que buscas no existe.</p>
+
+      <a href="#">Ir al inicio</a>
+    </>
+  );
+}
